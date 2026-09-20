87f4f7069b2b499c6c577c90d6678a63cdf7cd87
Dr.shubham <shubhamaryan.5432@gmail.com>
2026-09-20 20:59:04 +0530

    Update check_result.py

--- check_result.py
+++ check_result.py
@@ -82,7 +82,7 @@ def main():
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.9",
-    "Referer": "https://natboard.edu.in/",
+    "Referer": "https://natboard.edu.in/parinam/neetpg/index",
             }
             pdf_resp.raise_for_status()
             with open(fname, "wb") as f:
